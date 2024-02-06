2ce082ffaff3b048ed1bd1a79dc4b47aa5501064
Willyam Castro <willyam.android@gmail.com>
2024-02-06 20:47:45 -0300

    returning to project: adjust in comments and nomenclature

--- Alarme-project/wifi.h
+++ Alarme-project/wifi.h
@@ -6,14 +6,13 @@ void connectWifi(String wifiSSID, String wifiPassword) {
   // Let us connect to WiFi
   WiFi.begin(wifiSSID, wifiPassword);
 
- 
+  Serial.print("Connecting network");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   
-  //Serial.println(".......");
-  Serial.print(" WiFi Connected....IP Address: ");
+  Serial.print("Connected in " + wifiSSID + ". IP Address: ");
   Serial.println(WiFi.localIP());
 
 }
