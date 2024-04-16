f6658b1acf04ded2fd1328dcd780d671655ccb86
Willyam Castro <willyam.android@gmail.com>
2024-04-16 20:05:29 -0300

    set credentials

---
+++ Alarme-project/credentials.h
@@ -0,0 +1,15 @@
+// Set data Firebase;
+#define FIREBASE_HOST "<...>.firebaseio.com"
+#define FIREBASE_APIKEY "AIk8rlh1haUitYi2Pgd7o0q25p0FGARJd7qgANW"
+#define USER_EMAIL "<you account>"
+#define USER_PASSWD "<password account>"
+
+ // Set data connection wireless;
+#define WIFI_SSID "<wifi name>"
+#define WIFI_PASSWORD "<wifi password>"
+
+// Set data MQTT connection
+#define MQTT_SERVER "mqtt.tago.io"
+#define MQTT_PORT 1883
+#define MQTT_USER "Default"
+#define MQTT_PASSWORD "c725bfd2-05f1-4da6-8cfa-b4ca97198cfb"
